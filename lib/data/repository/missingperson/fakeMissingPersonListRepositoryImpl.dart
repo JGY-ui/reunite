47abeddced33138b8b4741bbea4bb3323b852265
ray <qaws58@naver.com>
2025-07-24 19:11:08 +0900

    오탈자 수정

--- lib/data/repository/missingperson/fakeMissingPersonListRepositoryImpl.dart
+++ lib/data/repository/missingperson/fakeMissingPersonListRepositoryImpl.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:reunite/ui/features/missingMap/models/model.dart';
-import 'missingPersonListRepository.dart';
+import 'missingPersonRepository.dart';
 
 class FakeMissingPersonsServiceImpl implements MissingPersonsRepository {
   Future<List<MissingPerson>> searchMissingPersons({
--- lib/data/service/missingPerson/fakeMissingPersonServiceImpl.dart
+++ lib/data/service/missingPerson/fakeMissingPersonServiceImpl.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:reunite/ui/features/missingMap/models/model.dart';
-import 'missingPersonListService.dart';
+import 'missingPersonService.dart';
 
-class FakeMissingPersonListServiceImpl implements MissingPersonListService {
+class FakeMissingPersonListServiceImpl implements MissingPersonService {
   Future<List<MissingPerson>> searchMissingPersons({
     required String name,
   }) async {
