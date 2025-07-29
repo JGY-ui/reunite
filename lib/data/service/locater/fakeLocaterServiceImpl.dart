81255c1a96aead40690490313c9ce33975007e03
ray <qaws58@naver.com>
2025-07-29 15:54:47 +0900

    locaterService 구현 및 테스트 추가

---
+++ lib/data/service/locater/fakeLocaterServiceImpl.dart
@@ -0,0 +1,9 @@
+import 'package:reunite/ui/features/missingMap/models/model.dart';
+import 'package:reunite/data/service/locater/locaterService.dart';
+
+class FakeLocaterServiceImpl implements LocaterService {
+  @override
+  Future<Coordinate> getCurrentLocation() async {
+    return Coordinate(lat: 37.5665851, long: 126.97820379999999);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:reunite/data/repository/location/fakeLocationRepositoryImpl.dart';
 import 'package:reunite/data/service/coding/fakeGeoCodingServiceImpl.dart';
 import 'package:reunite/data/service/coding/geocodingServiceImpl.dart';
+import 'package:reunite/data/service/locater/fakeLocaterServiceImpl.dart';
+import 'package:reunite/data/service/locater/locaterServiceimpl.dart';
 import 'package:reunite/permission/permission.dart';
 
 void main() {
@@ -46,17 +49,31 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   Future<void> _incrementCounter() async {
-    var geoCodingService = FakeGeoGeoCodingServiceImpl();
-    var coordinateFromAddress =
-        await geoCodingService.getCoordinateFromAddress('');
+    /*위치 값 서비스 테스트 코드*/
+    // var geoCodingService = FakeGeoGeoCodingServiceImpl();
+    // var coordinateFromAddress =
+    //     await geoCodingService.getCoordinateFromAddress('');
 
-    var geoCodingService_real_data = GeoCodingServiceImpl();
-    var coordinateFromAddress_real_data =
-        await geoCodingService.getCoordinateFromAddress('서울특별시 중구 세종대로 110');
+    // var geoCodingService_real_data = GeoCodingServiceImpl();
+    // var coordinateFromAddress_real_data =
+    //     await geoCodingService.getCoordinateFromAddress('서울특별시 중구 세종대로 110');
 
-    // 주소 변환 테스트 코드
+    // // 주소 변환 테스트 코드
+    // print(
+    //     'coordinateFromAddress : ${coordinateFromAddress.lat} / coordinateFromAddress_real_data : ${coordinateFromAddress_real_data.lat}');
+
+    /*위치 값 서비스 테스트 코드*/
+    var locaterService = FakeLocaterServiceImpl();
+    var getCurrentLocation = await locaterService.getCurrentLocation();
+
+    var locaterService_real_data = LocaterServiceImpl();
+    var getCurrentLocation_real_data =
+        await locaterService_real_data.getCurrentLocation();
+
     print(
-        'coordinateFromAddress : ${coordinateFromAddress.lat} / coordinateFromAddress_real_data : ${coordinateFromAddress_real_data.lat}');
+        'fake coordinateFromAddress : ${getCurrentLocation.lat} / coordinateFromAddress : ${getCurrentLocation.long}');
+    print(
+        'fake coordinateFromAddress : ${getCurrentLocation_real_data.lat} / coordinateFromAddress : ${getCurrentLocation_real_data.long}');
 
     setState(() {
       _counter++;
